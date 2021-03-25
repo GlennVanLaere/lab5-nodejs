94ab4b429fe476faa7b481f28445b4544e677327
GlennVanLaere <55496118+GlennVanLaere@users.noreply.github.com>
2021-03-25 15:28:51 +0100

    process added to listen

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use('/api/v1/messages', messageRouter);
 
 
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
